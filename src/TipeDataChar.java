d3fd8543bab9ebe57a48249e0de043b8af9a8a05
aldisypu <aldisypu@gmail.com>
2024-01-14 18:00:04 +0700

    belajar java dasar

---
+++ src/TipeDataChar.java
@@ -0,0 +1,14 @@
+public class TipeDataChar {
+  public static void main(String[] args) {
+
+
+    char o = 'O';
+    char d = 'D';
+    char i = 'I';
+
+    System.out.print(o);
+    System.out.print(d);
+    System.out.print(i);
+
+  }
+}
---
+++ src/TipeDataString.java
@@ -0,0 +1,16 @@
+public class TipeDataString {
+
+  public static void main(String[] args) {
+
+    String firstName = "Aldi Syah";
+    String lastName = "Putra";
+
+    String fullName = firstName + " " + lastName;
+
+    System.out.println(firstName);
+    System.out.println(lastName);
+
+    System.out.println(fullName);
+
+  }
+}
